526a16ee678a664c3e1c9d567e45ebe3186a8c63
tofutigerz <hfu3@ualberta.ca>
2022-03-23 10:04:41 -0600

    adds InterviewerListItem

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
-export default InterviewerListItem = () => {
+export default function InterviewerListItem(props) {
+  
   return (
-    <li className="interviewers__item">
+    <li 
+      className={classNames(
+        "interviewers__item",
+        {
+          'interviewers__item--selected': props.selected 
+        }
+      )}
+      onClick={() => props.setInterviewer(props.id)}
+    >
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.selected && props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
   );
 };
