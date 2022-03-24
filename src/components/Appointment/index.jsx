170cc6357f7736dd169a75b73ada84bb3f6a5bee
tofutigerz <hfu3@ualberta.ca>
2022-03-24 16:30:50 -0600

    makes use of useEffect and axios for fetching data

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,5 +1,7 @@
 import "components/Appointment/styles.scss";
 import React from "react";
+
+
 import Show from "./Show";
 import Header from "./Header";
 import Empty from "./Empty";
@@ -7,7 +9,7 @@ import Empty from "./Empty";
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {props.time}
+      <Header time={props.time} />
       {props.interview ? <Show interviewer={props.interview.interviewer} student={props.interview.student}/> : <Empty />}
     </article>
   );
