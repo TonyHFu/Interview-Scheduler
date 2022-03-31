017c67af8a26c9db0453d8fdaea107afc242609c
tofutigerz <hfu3@ualberta.ca>
2022-03-30 18:24:20 -0600

    refactors tests to use first and moves axios import in index

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -15,7 +15,7 @@ describe("Appointments", () => {
 	});
 
 	it("should edit an interview", () => {
-		cy.get("[alt=Edit]").click({ force: true });
+		cy.get("[alt=Edit]").first().click({ force: true });
 		cy.get("[data-testid=student-name-input]")
 			.clear()
 			.type("Lydia Miller-Jones");
@@ -27,8 +27,8 @@ describe("Appointments", () => {
 		cy.contains(".appointment__card--show", "Tori Malcolm");
 	});
 
-	it.only("should cancel an interview", () => {
-		cy.get("[alt=Delete]").click({ force: true });
+	it("should cancel an interview", () => {
+		cy.get("[alt=Delete]").first().click({ force: true });
 		cy.contains("Confirm").click();
 		cy.contains("Deleting").should("exist");
 		cy.contains("Deleting").should("not.exist");
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 import "index.scss";
 
 import Application from "components/Application";
-
 import axios from "axios";
 
 if (process.env.REACT_APP_API_BASE_URL) {
